ad17adb7d0ee364c1a0b0bb5fffdbfe9c37d746e
techsriman <tech.sriman.com>
2022-04-24 19:54:12 -0400

    uri

---
+++ src/main/java/com/urotaxi/UrotaxiApplication.java
@@ -0,0 +1,31 @@
+package com.urotaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class UrotaxiApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrotaxiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UrotaxiApplication.class);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index.html").setViewName("index");
+		registry.addViewController("/contact.html").setViewName("contact");
+		registry.addViewController("/service.html").setViewName("service");
+		registry.addViewController("/news.html").setViewName("news");
+		registry.addViewController("/about.html").setViewName("about");
+	}
+
+}
---
+++ src/test/java/com/urotaxi/UrotaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urotaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrotaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
